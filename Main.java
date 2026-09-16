5ac5a19f2448a19c913cc1e0c448aa92bf135e0c
Brent Evan Mondejar <mondejarbrent17@gmail.com>
2026-09-16 08:56:58 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+class Main{
+
+   public static void main(String[] args){
+   
+   Movie s1 = new Movie();
+   
+   s1.title = "Garden of words";
+   s1.genre = "Romance";
+   s1.duration = 45;
+   
+   s1.displayInfo();
+   
+   Movie s2 = new Movie();
+   
+   s2.title = "Voice of a distant star";
+   s2.genre = "Romance";
+   s2.duration = 55;
+   
+    s2.displayInfo();
+
+   
+   Movie s3 = new Movie();
+   
+   s3.title = "Into the forest of fireflies";
+   s3.genre = "Super natural";
+   s3.duration = 55;
+   
+    s3.displayInfo();
+
+   
+    }
+   }
